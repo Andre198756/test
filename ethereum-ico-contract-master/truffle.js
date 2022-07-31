bf8229aecb2e474899f64813a77e86e5fe37f617
Andre198756 <108952930+Andre198756@users.noreply.github.com>
2022-07-31 20:28:58 +0100

    Update truffle.js

--- ethereum-ico-contract-master/truffle.js
+++ ethereum-ico-contract-master/truffle.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "https://evm-t3.cronos.org/",
       port: 8545,
       network_id: "*" // Match any network id
     }
